87f326606c8d3b7f13adbc94d99fdb66af3c7deb
Manik Roy <pixnum@gmail.com>
2026-08-01 20:03:07 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE = 'spasht-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', function(e){
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(ASSETS); }));
+});
+
+self.addEventListener('activate', function(e){
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+    }).then(function(){ return self.clients.claim(); })
+  );
+});
+
+self.addEventListener('fetch', function(e){
+  if(e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(function(cached){
+      var network = fetch(e.request).then(function(res){
+        if(res && res.status === 200 && res.type === 'basic'){
+          var copy = res.clone();
+          caches.open(CACHE).then(function(c){ c.put(e.request, copy); });
+        }
+        return res;
+      }).catch(function(){ return cached; });
+      return cached || network;
+    })
+  );
+});
